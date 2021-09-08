f204c58fa5f31ffd04b8285460cb53deb416e69c
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-08 14:48:11 -0400

    Usando Componente

---
+++ masterclass1/src/components/Cabecalho.jsx
@@ -0,0 +1,7 @@
+const Cabecalho = () => (
+  <header>
+    <h1>Fundamentos de Nextjs & ReactJS</h1>
+  </header>
+);
+
+export default Cabecalho;
---
+++ masterclass1/src/pages/exemplo.jsx
@@ -0,0 +1,10 @@
+import Cabecalho from '../components/Cabecalho';
+
+const Exemplo = () => (
+  <>
+    <Cabecalho />
+    <h2>Exemplo</h2>
+  </>
+);
+
+export default Exemplo;
