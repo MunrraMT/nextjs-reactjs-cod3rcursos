bea20ddce00c02c59140af2ceef6a345753b67e6
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-08 14:41:27 -0400

    CSS Modularizado

---
+++ masterclass1/src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Estiloso.module.css';
+
+const Estiloso = () => (
+  <div className={styles.vermelho}>
+    <h1>Estilo usando CSS Módulos</h1>
+  </div>
+);
+
+export default Estiloso;
