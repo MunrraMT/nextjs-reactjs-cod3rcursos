4191245d9c025e5165043c49a36a1865135f67ce
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-08 12:54:51 -0400

    Entendendo JSX

--- masterclass1/src/pages/index.jsx
+++ masterclass1/src/pages/index.jsx
@@ -1,5 +1,10 @@
-const Home = () => {
-  return 'inicio';
+const Inicio = () => {
+  return (
+    <div>
+      <h1>Fundamentos de NextJS & ReactJS</h1>
+      <h2>Vamos estudar esse framework</h2>
+    </div>
+  );
 };
 
-export default Home;
+export default Inicio;
