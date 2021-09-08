1861afec3a6c15f34679f1d01ac8eeca3e527ad2
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-08 14:25:45 -0400

    Integração JavaScript/JSX

--- masterclass1/src/pages/index.jsx
+++ masterclass1/src/pages/index.jsx
@@ -1,10 +1,8 @@
-const Inicio = () => {
-  return (
-    <div>
-      <h1>Fundamentos de NextJS & ReactJS</h1>
-      <h2>Vamos estudar esse framework</h2>
-    </div>
-  );
-};
+const Inicio = () => (
+  <div>
+    <h1>Fundamentos de NextJS & ReactJS</h1>
+    <h2>Vamos estudar esse framework</h2>
+  </div>
+);
 
 export default Inicio;
---
+++ masterclass1/src/pages/jsx.jsx
@@ -0,0 +1,22 @@
+const Jsx = () => {
+  const titulo = <h1>JSX é um conceito central</h1>;
+
+  const a = 4;
+  const b = 3;
+
+  const subTitulo = () => <h2>{'muito legal'.toUpperCase()}</h2>;
+
+  return (
+    <main>
+      {titulo}
+
+      <h2>{a * b}</h2>
+
+      {subTitulo()}
+
+      <code>{JSON.stringify({ nome: 'João', idade: 30 })}</code>
+    </main>
+  );
+};
+
+export default Jsx;
