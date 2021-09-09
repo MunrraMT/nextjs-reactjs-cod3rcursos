0cb0000f7e816620e6023e345a5e5c0c0f8b2db3
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-09 15:23:13 -0400

    Usando API 2

---
+++ masterclass1/src/pages/api/clientes/[codigo].js
@@ -0,0 +1,11 @@
+const handler = (req, res) => {
+  const codigo = req.query.codigo;
+
+  res.status(200).json({
+    id: codigo,
+    nome: `André ${codigo}`,
+    email: `andre@${codigo}.com.br`,
+  });
+};
+
+export default handler;
