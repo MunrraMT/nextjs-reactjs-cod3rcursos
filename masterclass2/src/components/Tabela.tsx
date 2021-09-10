b4a4c42a1c4581e0019b0eaa634edf701b7d4ae8
Andre Rodrigues Melgaço dos Santos <dev-asantos@outlook.com>
2021-09-10 13:23:52 -0400

    Componente Tabela 2

--- masterclass2/src/components/Tabela.tsx
+++ masterclass2/src/components/Tabela.tsx
@@ -2,11 +2,11 @@ import Cliente from '../core/Cliente';
 
 const Tabela = ({ clientes }: { clientes: Cliente[] }) => {
   const renderizarCabecalho = () => (
-    <thead>
-      <tr>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Idade</th>
+    <thead className="bg-gradient-to-r from-purple-500 to-purple-800 text-gray-100">
+      <tr className="text-left">
+        <th className="p-4">Código</th>
+        <th className="p-4">Nome</th>
+        <th className="p-4">Idade</th>
       </tr>
     </thead>
   );
@@ -14,17 +14,22 @@ const Tabela = ({ clientes }: { clientes: Cliente[] }) => {
   const renderizarCorpo = () => (
     <tbody>
       {clientes?.map(({ id, nome, idade }, index) => (
-        <tr key={id}>
-          <td>{id}</td>
-          <td>{nome}</td>
-          <td>{idade}</td>
+        <tr
+          className={`text-left ${
+            index % 2 === 0 ? 'bg-purple-200' : 'bg-purple-100'
+          }`}
+          key={id}
+        >
+          <td className="p-4">{id}</td>
+          <td className="p-4">{nome}</td>
+          <td className="p-4">{idade}</td>
         </tr>
       ))}
     </tbody>
   );
 
   return (
-    <table>
+    <table className="w-full rounded-xl overflow-hidden">
       {renderizarCabecalho()}
       {renderizarCorpo()}
     </table>
